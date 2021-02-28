01d8c3f4f4566184035783475a9552e36547ea5b
farhaan-ali <farhaan@farhaanali.com>
2021-02-28 15:53:46 +0000

    Contract: Update Greeter contract to set greeting message.
    This allows the user to be able to change the greeting to whatever
    they desire when interacting with the blockchain.

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -5,14 +5,31 @@ contract("Greeter", () => {
     const greeter = await GreeterContract.deployed();
     assert(greeter, "contract was not deployed");
   });
-});
 
-describe("greet()", () => {
-  it("returns 'Hello, world!'", async () => {
-    const greeter = await GreeterContract.deployed();
-    const expected = "Hello, World!";
-    const actual = await greeter.greet();
+  describe("greet()", () => {
+    it("returns 'Hello, world!'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
 
-    assert.equal(actual, expected, "greeted with Hello, World!'");
+      assert.equal(actual, expected, "greeted with Hello, World!'");
+    });
   });
+  
 });
+
+
+
+contract("Greeter: updated greeting", () => {
+  describe("setGreeting(string)", () => {
+    it("sets greeting that was passed in the string", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hi there!"
+
+      await greeter.setGreeting(expected)
+      const actual = await greeter.greet()
+
+      assert.equal(actual, expected, "greeting was not updated")
+    })
+  })
+})
